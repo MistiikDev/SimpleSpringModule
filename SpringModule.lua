3c376c3b791d8d9e627b52ea8965622ba7c49feb
Mistiik <91028158+MistiikDev@users.noreply.github.com>
2021-09-25 17:05:23 +0200

    SpringModule.lua

---
+++ SpringModule.lua
@@ -0,0 +1,41 @@
+--[[
+	Scripter: Mistiik (XRHTech)
+]]--
+
+local spring = {}
+
+spring.__index = spring
+
+function spring.new(dampling, friction)
+	local self = setmetatable({
+		-- Hooks Constants
+		k = dampling or 0.3,
+		f = friction or 0.5,
+		
+		-- Vectors
+		
+		position = Vector3.new(),
+		velocity = Vector3.new(),
+		target = Vector3.new()
+		
+	}, spring)
+	
+	return self
+end
+
+function spring:set_target(v3)
+	self.target = v3
+end
+
+function spring:update()
+	local d = self.target - self.position
+	local force = d * self.k
+
+	self.velocity = (self.velocity * (1-self.f)) + force;
+
+	self.position += self.velocity
+
+	return self.position
+end
+
+return spring
